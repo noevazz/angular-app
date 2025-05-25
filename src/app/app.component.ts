d56c2fd41bcc39eb5cda467570818ae4b7c25001
noevazz <jonovagu@gmail.com>
2025-05-25 10:09:42 -0600

    added hero-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeroItemComponent } from './components/hero-item/hero-item.component';
+import { HeroListComponent } from "./components/hero-list/hero-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroItemComponent],
+  imports: [HeroListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'HERO APP';
 }
